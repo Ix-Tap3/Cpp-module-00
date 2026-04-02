f1b8d4b1f5c9bec358f867e4ecdcc7d4e2fa073c
pcaplat <patxicaplat.pro@gmail.com>
2026-04-02 10:47:21 +0200

    implement setters and getters for Contact class

---
+++ ex01/Contact.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pcaplat <pcaplat@42angouleme.fr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/02 10:23:10 by pcaplat           #+#    #+#             */
+/*   Updated: 2026/04/02 10:46:34 by pcaplat          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Contact.hpp"
+
+void	Contact::setFirstName(std::string name){_firstName = name;}
+void	Contact::setLastName(std::string name){_lastName = name;}
+void	Contact::setNickname(std::string name){_nickname = name;}
+void	Contact::setPhoneNumber(std::string phoneNumber){_phone = phoneNumber;}
+void	Contact::setSecret(std::string secret){_secret = secret;}
+
+std::string	Contact::getFirstName(void){return (_firstName);}
+std::string	Contact::getLastName(void){return (_lastName);}
+std::string	Contact::getNickname(void){return (_nickname);}
+std::string	Contact::getPhoneNumber(void){return (_phone);}
+std::string	Contact::getSecret(void){return (_secret);}
