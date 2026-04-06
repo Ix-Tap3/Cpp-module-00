94305de9f4a7e5c3240f9304d56cff7a12f18b55
pcaplat <patxicaplat.pro@gmail.com>
2026-04-06 14:18:49 +0200

    modify phonebook class

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: pcaplat <pcaplat@42angouleme.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/02 09:47:16 by pcaplat           #+#    #+#             */
-/*   Updated: 2026/04/02 10:34:45 by pcaplat          ###   ########.fr       */
+/*   Updated: 2026/04/02 11:15:41 by pcaplat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ private:
 	Contact	contacts[8];
 
 public:
-	void	addContact(Contact &contact);
+	void	add(Contact &contact);
 	void	search(std::string name);
 	void	exit(void);
 };
